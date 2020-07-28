58b95d0c42025c627ac5883f5587acb9aab59e9b
Jason <jasonathena0604@gmail.com>
2020-07-28 19:34:39 -0400

    Please enter the commit message for your changes. Lines starting

--- ccc/assigning_parteners.py
+++ ccc/assigning_parteners.py
@@ -5,11 +5,11 @@ first = input().split()
 second = input().split()
 
 i = 0
-state = "good"
-while i < n and state == "good":
+status = "good"
+while i < n and status == "good":
     position = first.index(second[i])
     if first[i] != second[position] or position == i:
         state = "bad"
     i += 1
 
-print(state)
+print(status)
